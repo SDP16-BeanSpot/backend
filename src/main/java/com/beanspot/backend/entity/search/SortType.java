52cea8f98d647f75a5a83650d8cd3b246d4a4987
HaSam <ssam8610@naver.com>
2026-02-22 18:44:28 +0900

    feat: 공고 DTO 및 ES 매핑 최신화

--- src/main/java/com/beanspot/backend/entity/search/SortType.java
+++ src/main/java/com/beanspot/backend/entity/search/SortType.java
@@ -1,7 +1,8 @@
 package com.beanspot.backend.entity.search;
 
 public enum SortType {
-    POPULAR,
-    LATEST,
-    DISTANCE
+    POPULAR,   // 인기순
+    LATEST,    // 최신순
+    DISTANCE,  // 거리순
+    RECOMMENDED // 추천순
 }
