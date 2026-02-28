2e05e301f9640f8e4799a8245cb0c9da814fca78
hyunbin7664 <s9476721@naver.com>
2026-02-28 20:44:28 +0900

    refactor: 리액션 카운트 필드 추가 및 데이터 구조 최적화

--- src/main/java/com/beanspot/backend/dto/chat/ChatMessageDto.java
+++ src/main/java/com/beanspot/backend/dto/chat/ChatMessageDto.java
@@ -1,6 +1,7 @@
 package com.beanspot.backend.dto.chat;
 
 import com.beanspot.backend.entity.ChatMessageType;
+import jakarta.persistence.Column;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
 import lombok.*;
@@ -12,11 +13,10 @@ import lombok.*;
 @AllArgsConstructor
 public class ChatMessageDto {
 
-    @Enumerated(EnumType.STRING)
     private ChatMessageType type;
     private String roomId;
     private String sender;
     private String message;
-
+    private int reactionCount = 0;
 
 }
